2e2616c687cb9b6fe97369b78fbc44a7d245091d
Flavio de Sousa <fl@viodesousa.com>
2014-03-08 13:54:58 -0300

    Made it shorter and slightly smarter

--- decoder.c
+++ decoder.c
@@ -1,1 +1,1 @@
-main(){int c,b=0,m=8;while((c=getchar())>=0){c-=48;if((c&1)==c)b=((b<<1)|c),m--;if(!m)putchar(b),m=8;}}
+main(){int c,b=0,m=8;while((c=getchar())>=0){b=((b<<1)|c&1);if(!--m)putchar(b),b=0,m=8;}}
